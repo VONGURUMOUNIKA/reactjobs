fb9dc62790ecce4ebb6bd3a924fc3c6941f90484
VONGURUMOUNIKA <vongurumounika52@gmail.com>
2025-04-29 17:55:08 -0500

    style changes

--- src/Components/HomeCards.jsx
+++ src/Components/HomeCards.jsx
@@ -8,9 +8,9 @@ const HomeCards = () => {
     <div className="container-xl lg:container m-auto">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
        <Card>
-       <h2 className="text-2xl font-bold">For Developers</h2>
+       <h2 className="text-2xl font-bold">For Job Seekers</h2>
           <p className="mt-2 mb-4">
-            Browse our React jobs and start your career today
+            Browse  jobs and start your career today
           </p>
           <Link
             to="/jobs"
@@ -20,14 +20,14 @@ const HomeCards = () => {
           </Link>
        </Card>
           
-        <Card bg='bg-indigo-100'> 
-        <h2 className="text-2xl font-bold">For Employers</h2>
+        <Card bg='bg-teal-200'> 
+        <h2 className="text-2xl font-bold">For Recruiters</h2>
           <p className="mt-2 mb-4">
             List your job to find the perfect developer for the role
           </p>
           <Link
             to="/add-job"
-            className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
+            className="inline-block bg-teal-700 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
           >
             Add Job
           </Link>
