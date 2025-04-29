136b4c8875b94f4073a5dbf82acc5ae99e81bf55
VONGURUMOUNIKA <vongurumounika52@gmail.com>
2025-04-29 17:54:58 -0500

    title and style changes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Hero = ({title = 'Beacome a React Dev', subtitle='Find the React job that fits your skill set'}) => {
+const Hero = ({title = 'Find your Next Career Move', subtitle='Where talent meets opportunity-One Platform to Hire and Get Hired '}) => {
   return (
-    <section className="bg-indigo-700 py-20 mb-4">
+    <section className="bg-teal-950 py-20 mb-4">
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
       <div className="text-center">
         <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
